e3790ab893e13bfd4b70eb14f64b09b3f12bafee
actapia <andrew.tapia@uky.edu>
2023-03-01 23:22:41 -0500

    Add parallel version.

---
+++ select_top_sets_all.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+function select_top_sets {
+    perl select_top_sets.pl --top="$top_n" --pattern="$pattern" --transcripts="$1/$transcript_fn" > "$out_dir/$(basename "$1")_top.fasta"
+}
+# DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# source optionsh.sh
+# declare -A MYARGS
+# parse_args $0 MYARGS "$@"
+# parse_result=$?
+# if [ $parse_result -ne 0 ]; then
+#     if [ $parse_result -eq 101 ]; then
+# 	exit 0
+#     else
+# 	exit $parse_result
+#     fi
+# fi
+declare -a dirlist
+transcript_fn="transcripts.fasta"
+out_dir="$PWD"
+top_n=10000
+pattern="^.*cov_([0-9]+(?:\.[0-9]+))_g([0-9]+)_i([0-9]+)"
+while [ "$#" -gt 0 ]; do
+    case "$1" in
+	"-t" | "--transcripts")
+	   shift;
+	   transcript_fn="$1";
+	   ;;
+	"-n" | "--top-n")
+	    shift;
+	    top_n="$1"
+	    ;;
+	"-o" | "--out-dir")
+	    shift;
+	    out_dir="$1"
+	    ;;
+	"-p" | "--pattern")
+	    shift;
+	    pattern="$1"
+	    ;;
+	*)
+	    if ! [ -d "$1" ]; then
+		>&2 echo "Directory $1 does not exist."
+		exit 1
+	    fi
+	    dirlist+=("$1")
+	    ;;
+    esac
+    shift
+done
+export transcript_fn
+export out_dir
+export top_n
+export pattern
+export -f select_top_sets
+parallel "-j$(($(nproc)-1))" select_top_sets ::: "${dirlist[@]}"
